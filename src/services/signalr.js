ae1a99fd25555373a4a6c21fdf9280e84b7926d6
sarban <sarban7@mail.ru>
2026-02-05 01:37:34 +0300

    добавлен signalr.js

---
+++ src/services/signalr.js
@@ -0,0 +1,62 @@
+import * as signalR from '@microsoft/signalr';
+
+const HUB_URL = 'https://localhost:7104/chathub';
+
+class SignalRService{
+    constructor(){
+        this.connection = null;
+    }
+
+    // Подключение к SignalR Hub
+    createConnection(token){
+        this.connection = new signalR.HubConnectionBuilder()
+        .withUrl(HUB_URL, {
+            accessTokenFactory: () => token,
+        })
+        .withAutomaticReconnect()
+        .build();
+    }
+
+    // Запуск подключения
+    async start(){
+        if(this.connection){
+            try{
+                await this.connection.start();
+                console.log('SignalR подключен');
+            } catch(err){
+                console.error('Ошибка подключения SignalR:', err);
+            }
+        }
+    }
+
+    // Остановить подключение
+    async stop() {
+        if(this.connection) {
+            await this.connection.stop();
+            console.log('SignalR отключен');
+        }
+    }
+
+    // Подписаться на событие "ReceiveMessage"
+    onUserStatusChange(callback){
+        if (this.connection){
+            this.connection.on('UserStatusChange', callback);
+        }
+    }
+
+    // Подписаться на событие "UserTyping"
+    onUserTyping(callback){
+        if(this.connection){
+            this.connection.on('UserTyping', callback);
+        }
+    }
+
+    // Отправить индикатор "печатает..."
+    async sendTyping(recipientId, isTyping){
+        if(this.connection){
+            await this.connection.invoke('Typing', recipientId, isTyping);
+        }
+    }
+}
+
+export default new SignalRService();
